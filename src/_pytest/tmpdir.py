3d1f1cad5ebe162c52ba21176a05777209daf2b5
tserg <8017125+tserg@users.noreply.github.com>
2024-03-24 22:59:07 +0800

    Improve sentence in marks documentation (#12157)

--- src/_pytest/tmpdir.py
+++ src/_pytest/tmpdir.py
@@ -111,7 +111,7 @@ class TempPathFactory:
     def _ensure_relative_to_basetemp(self, basename: str) -> str:
         basename = os.path.normpath(basename)
         if (self.getbasetemp() / basename).resolve().parent != self.getbasetemp():
-            raise ValueError(f"{basename} is not a normalized and relative path")
+            raise RuntimeError(f"{basename} is not a normalized and relative path")
         return basename
 
     def mktemp(self, basename: str, numbered: bool = True) -> Path:
